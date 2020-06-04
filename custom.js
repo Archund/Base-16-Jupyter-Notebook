a89efb11d060f7e981cae922d81cb477dd405d95
Archund <jarchund@gmail.com>
2020-06-03 20:22:32 -0500

    Misc fixes

--- custom.js
+++ custom.js
@@ -6,22 +6,22 @@ var scrollPosition      = notebook.scrollTop;
 var headerHeight        = header.offsetHeight;
 
 
-notebook.onscroll = function () {
-    var newScrollPosition = notebook.scrollTop;
+// notebook.onscroll = function () {
+//     var newScrollPosition = notebook.scrollTop;
     
-    // Hide
-    if (scrollPosition > newScrollPosition) {
-        // header.style.transform = "translateY(0)";
-        // header.style.maxHeight = headerHeight + "px";
-        // header.style.paddingBottom = "0";
-        header.style.display = "initial";
+//     // Hide
+//     if (scrollPosition > newScrollPosition) {
+//         // header.style.transform = "translateY(0)";
+//         // header.style.maxHeight = headerHeight + "px";
+//         // header.style.paddingBottom = "0";
+//         header.style.display = "initial";
 
-    // Show
-    } else {
-        // header.style.transform = "translateY(-9999px)";
-        // header.style.maxHeight = "0";
-        // header.style.paddingBottom = "8";
-        header.style.display = "none";
-    }
-    scrollPosition = newScrollPosition;
-}
+//     // Show
+//     } else {
+//         // header.style.transform = "translateY(-9999px)";
+//         // header.style.maxHeight = "0";
+//         // header.style.paddingBottom = "8";
+//         header.style.display = "none";
+//     }
+//     scrollPosition = newScrollPosition;
+// }
